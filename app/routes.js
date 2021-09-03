22627a914a8ab2f6106fd12c027139a20adfcf95
Jon Maher <jonmaher@me.com>
2021-09-03 12:40:03 +0100

    round 2

--- app/routes.js
+++ app/routes.js
@@ -256,11 +256,270 @@ router.post('/round-1/employment-status-check', function (req, res) {
     const empstat = req.session.data['employment-status']
 
     if (empstat === 'Unemployed') {
-        res.redirect('/round-1/universal-credit/childcare-providers')
+        res.redirect('/round-1/universal-credit/childcare-provider')
     } else {
         res.redirect('/round-1/universal-credit/parent-work-hours')
     }
 })
 
+//ROUND 2 ROUTES
+// childcare calc account logic
+router.post('/round-2/sign-in-or-create-account-form', function (req, res) {
+
+    // The content in the "" is the name of the radio button
+    var option = req.session.data['signin-create-account-radios']
+
+    // The content in the "" is the value of the radio button
+    if (option === 'create-account') {
+        // Send user to this page
+        res.redirect('/round-2/account/create-account-enter-email')
+    } else if (option === 'signin') {
+        // Or send user to this page
+        res.redirect('/round-2/account/sign-in')
+    } else {
+        // Or send user to this page
+        res.redirect('/round-2/childcare-calculator/what-youll-need')
+    }
+})
+
+// universal credit account logic
+router.post('/round-2/sign-in-or-create-account-form-2', function (req, res) {
+
+    // The content in the "" is the name of the radio button
+    var option = req.session.data['signin-create-account-radios']
+
+    // The content in the "" is the value of the radio button
+    if (option === 'create-account') {
+        // Send user to this page
+        res.redirect('/round-2/account/create-account-enter-email')
+    } else if (option === 'signin') {
+        // Or send user to this page
+        res.redirect('/round-2/account/sign-in')
+    } else {
+        // Or send user to this page
+        res.redirect('/round-2/universal-credit/name')
+    }
+})
+
+// replace passport account logic
+router.post('/round-2/sign-in-or-create-account-form-4', function (req, res) {
+
+    // The content in the "" is the name of the radio button
+    var option = req.session.data['signin-create-account-radios']
+
+    // The content in the "" is the value of the radio button
+    if (option === 'create-account') {
+        // Send user to this page
+        res.redirect('/round-2/account/create-account-enter-email')
+    } else if (option === 'signin') {
+        // Or send user to this page
+        res.redirect('/round-2/account/sign-in')
+    } else {
+        // Or send user to this page
+        res.redirect('/round-2/replace-your-passport/do-you-live-in-the-uk')
+    }
+})
+
+// masthead account logic
+router.post('/round-2/sign-in-or-create-account-form-3', function (req, res) {
+
+    // The content in the "" is the name of the radio button
+    var option = req.session.data['signin-create-account-radios']
+
+    // The content in the "" is the value of the radio button
+    if (option === 'create-account') {
+        // Send user to this page
+        res.redirect('/round-2/account/create-account-enter-email')
+    } else if (option === 'signin') {
+        // Or send user to this page
+        res.redirect('/round-2/account/sign-in')
+    } else {
+        // Or send user to this page
+        res.redirect('/round-2/account/check-account-enter-email')
+    }
+})
+
+// round 1 Resume application - NOT USED
+// sign-in-to-resume.html [account check]
+router.post('/round-2/check-account-resume', function (req, res) {
+    email = req.session.data['email-address']
+    oldEmail = req.session.data['email-address-used'] || ""
+    signin = req.session.data['sign-in'] || ""
+    console.log('new email is: ' + email)
+    console.log('previous email is: ' + oldEmail)
+    if (email === oldEmail && email !== '') {
+        res.redirect('/round-2/enter-password')
+    } else {
+        if (signin === "resume") {
+            //oldEmail = email
+            //req.session.data['email-address-used'] = email
+            res.redirect('/round-2/account-not-found-resume')
+        } else {
+            oldEmail = email
+            req.session.data['email-address-used'] = email
+            res.redirect('/round-2/check-email')
+        }
+    }
+})
+
+// Create an Account or Sign in page Check if you have a GOV.UK Account route
+// create-account-enter-email.html
+router.post('/round-2/create-account', function (req, res) {
+    email = req.session.data['email-address']
+    oldEmail = req.session.data['email-address-used'] || ""
+    signin = req.session.data['sign-in'] || ""
+    console.log('v5 new email is: ' + email)
+    console.log('previous email is: ' + oldEmail)
+    if (email === oldEmail && email !== '') {
+        res.redirect('/round-2/account/enter-password')
+    } else {
+        if (signin === "resume") {
+            res.redirect('/round-2/account/account-not-found')
+        } else {
+            oldEmail = email
+            req.session.data['email-address-used'] = email
+            res.redirect('/round-2/account/check-email')
+        }
+    }
+})
+
+// Create an Account or Sign in page Sign in route
+// sign-in.html
+router.post('/round-2/check-account-sign-in', function (req, res) {
+    email = req.session.data['email-address']
+    oldEmail = req.session.data['email-address-used'] || ""
+    signin = req.session.data['sign-in'] || ""
+    console.log('new email is: ' + email)
+    console.log('previous email is: ' + oldEmail)
+    if (email === oldEmail && email !== '') {
+        res.redirect('/round-2/account/enter-password')
+    } else {
+        if (signin === "signin") {
+            res.redirect('/round-2/account/account-not-found-sign-in')
+        } else {
+            oldEmail = email
+            req.session.data['email-address-used'] = email
+            res.redirect('/round-2/account/account-not-found-sign-in')
+        }
+    }
+})
+
+// Create an Account or Sign in page Check if you have a GOV.UK Account route
+// check-account-enter-email.html
+router.post('/round-2/check-account', function (req, res) {
+    email = req.session.data['email-address']
+    oldEmail = req.session.data['email-address-used'] || ""
+    signin = req.session.data['sign-in'] || ""
+    console.log('new email is: ' + email)
+    console.log('previous email is: ' + oldEmail)
+    if (email === oldEmail && email !== '') {
+        res.redirect('/round-2/account/enter-password')
+    } else {
+        if (signin === "check") {
+            res.redirect('/round-2/account/account-not-found')
+        } else {
+            oldEmail = email
+            req.session.data['email-address-used'] = email
+            res.redirect('/round-2/account/account-not-found')
+        }
+    }
+})
+
+// round 1 Childcare calc add additional child 1 
+// child-1-details.html
+router.post('/round-2/child-1-add', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+
+    const addchild1 = req.session.data['add-child-1']
+
+    if (addchild1 === 'yes') {
+        res.redirect('/round-2/childcare-calculator/child-2-details')
+    } else {
+        res.redirect('/round-2/childcare-calculator/childcare-costs')
+    }
+})
+
+// round 1 Childcare calc add additional child 2 
+// child-2-details.html
+router.post('/round-2/child-2-add', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+
+    const addchild2 = req.session.data['add-child-2']
+
+    if (addchild2 === 'yes') {
+        res.redirect('/round-2/childcare-calculator/child-3-details')
+    } else {
+        res.redirect('/round-2/childcare-calculator/childcare-costs')
+    }
+})
+
+// round 1 Childcare calc add additional child 3 
+// child-3-details.html
+router.post('/round-2/child-3-add', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+
+    const addchild3 = req.session.data['add-child-3']
+
+    if (addchild3 === 'yes') {
+        res.redirect('/round-2/childcare-calculator/childcare-costs')
+    } else {
+        res.redirect('/round-2/childcare-calculator/childcare-costs')
+    }
+})
+
+// round 1 Childcare calc live with partner 
+// do-you-live-with-partner.html
+router.post('/round-2/partner-check', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+
+    const partner = req.session.data['partner-check']
+
+    if (partner === 'yes') {
+        res.redirect('/round-2/childcare-calculator/are-you-in-paid-work')
+    } else {
+        res.redirect('/round-2/childcare-calculator/are-you-in-paid-work')
+    }
+})
+
+
+// round 1 Childcare calc are you in paid work
+// are-you-in-paid-work.html
+router.post('/round-2/paid-work-check', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+
+    const empstat = req.session.data['employment-status']
+
+    if (empstat === 'Unemployed') {
+        res.redirect('/round-2/childcare-calculator/check-your-answers')
+    } else {
+        res.redirect('/round-2/childcare-calculator/parent-work-hours')
+    }
+})
+
+// round 1 UC employment status check
+// employment-status.html
+router.post('/round-2/employment-status-check', function (req, res) {
+    // Get the answer from session data
+    // The name between the quotes is the same as the 'name' attribute on the input elements
+    // However in JavaScript we can't use hyphens in variable names
+
+    const empstat = req.session.data['employment-status']
+
+    if (empstat === 'Unemployed') {
+        res.redirect('/round-2/universal-credit/childcare-provider')
+    } else {
+        res.redirect('/round-2/universal-credit/parent-work-hours')
+    }
+})
 
 module.exports = router
